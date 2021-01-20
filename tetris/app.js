cd355bdd0a14900cb7dc41703021a4db5267eccb
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-01-20 11:59:19 +0530

    keydown & start/stop functionality added

--- tetris/app.js
+++ tetris/app.js
@@ -2,9 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const grid = document.querySelector('.grid')
     let squares = Array.from(grid.querySelectorAll('div'))
+    const startBtn = document.querySelector('.startBtn')
     const width = 10
     const height = 20
+    const size = width * height
+
+    let timerId
     let currentPosition = 4
+
+    //assign functions to keyCode
+    function control(e) {
+        if(e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 38) {
+            rotate()
+        } else if (e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keydown', control)
 
     // The Tetrominoes
 
@@ -68,10 +86,22 @@ document.addEventListener('DOMContentLoaded', () => {
     //move down shape
     function moveDown() {
         undraw()
-        currentPosition = currentPosition+= width
+        currentPosition = currentPosition += width
         draw()
         freeze()
     }
+
+    startBtn.addEventListener('click', () => {
+        if(timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+        }
+    })
+    
 
     // move left and prevent collisions with shapes moving left
     function moveRight() {
@@ -85,7 +115,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function moveLeft() {
-        
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+        if(!isAtLeftEdge)   currentPosition -= 1
+        if(current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition += 1
+        }
+        draw()
     }
 
+    //rotate Tetromino
+    function rotate() {
+        undraw()
+        currentPosition++
+        if(currentRotation === current.length) {
+            currentRotation = 0
+        }
+        current = theTetrominoes[random][currentRotation]
+        draw()
+    }
+
+    draw()
 })
