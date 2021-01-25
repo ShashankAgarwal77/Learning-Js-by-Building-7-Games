d2514282ae8e9ba6f67944b6b57f3a1955ba8d8a
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-01-25 22:46:22 +0530

    tetris added

--- tetris/app.js
+++ tetris/app.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const grid = document.querySelector('.grid')
+    const scoreDisplay = document.querySelector('.score-display')
+    const linesDisplay = document.querySelector('.line-display')
+    // const displaySquares = document.querySelectorAll('.previous-grid div')
     let squares = Array.from(grid.querySelectorAll('div'))
     const startBtn = document.querySelector('.startBtn')
     const width = 10
     const height = 20
-    const size = width * height
+    // const size = width * height
 
     let timerId
+    let score = 0
+    let lines = 0
     let currentPosition = 4
+    let currentIndex = 0
 
     //assign functions to keyCode
     function control(e) {
@@ -64,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 
-    //Randomly select Terinubo
+    //Randomly select Tetrominoes
     let random = Math.floor(Math.random()*theTetrominoes.length)
     let currentRotation = 0
     let current = theTetrominoes[random][currentRotation]
@@ -90,17 +96,6 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
         freeze()
     }
-
-    startBtn.addEventListener('click', () => {
-        if(timerId) {
-            clearInterval(timerId)
-            timerId = null
-        } else {
-            draw()
-            timerId = setInterval(moveDown, 1000)
-            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
-        }
-    })
     
 
     // move left and prevent collisions with shapes moving left
@@ -135,5 +130,90 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
     }
 
-    draw()
+    // show the previous tetromino is displaySquares
+    // const displayWidth = 4
+    // const displayIndex = 0
+    // let nextRandom = 0
+
+    // const smallTetrominoes = [
+    //     [1, displayWidth+1, displayWidth*2+1, 2] /* lTetromino */
+    //     [0, displayWidth, displayWidth+1, displayWidth*2+1] /* zTetromino */
+    //     [1, displayWidth, displayWidth+1, displayWidth+2] /* tTetromino */
+    //     [0, 1, displayWidth+1, displayWidth+1] /* oTetromino */
+    //     [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] /* iTetromino */
+
+    // ] 
+
+    // function displayShape() {
+    //     displaySquares.forEach(square => {
+    //         square.classList.remove('block')
+    //     })
+    //     smallTetrominoes[nextRandom].forEach(
+    //         index => {
+    //             displaySquares[displayIndex + index].classList.add('block')
+    //     })
+    // }
+
+    // displayShape()
+
+    //freeze the shape
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('block3') || squares[currentPosition + index + width].classList.contains('block2'))) {
+            current.forEach(index => squares[index + currentPosition].classList.add('block2'))
+
+            random = nextRandom
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentPosition]
+            currentPosition = 4
+            draw()
+            // displayShape()
+            gameOver()
+            addScore()
+        } 
+    }
+
+    startBtn.addEventListener('click', () => {
+        if(timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+        }
+    })
+
+    // game over
+    function gameOver() {
+        if(current.some
+            (index => squares[currentPosition + index].classList.contains('block2'))) {
+                scoreDisplay.innerHTML = 'end'
+                clearInterval(timerId)
+            }
+    }
+
+    // add score
+    function addScore() {
+        for(currentIndex = 0; currentIndex < 199; currentIndex += width) {
+            const row = [currentIndex, currentIndex+1, currentIndex+2, currentIndex+3, currentIndex+4, currentIndex+5, currentIndex+6, currentIndex+7, currentIndex+8, currentIndex+9]
+
+            if(row.every(index => squares[index].classList.contains('block2'))) {
+                score += 10
+                lines += 1
+                scoreDisplay.innerHTML = score
+                linesDisplay.innerHTML = lines
+                row.forEach( index => {
+                    squares[index].classList.remove('block2') || squares[index].classList.remove('block')
+                })
+
+                //splice array
+                const sqauresRemoved = squares.splice(currentIndex, width)
+                squares = sqauresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+
+        }
+    }
+
+
 })
